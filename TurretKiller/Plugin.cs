9e04bb9011803831088f401f49737940a8be9d1a
5-23 <yhanbyeol6@gmail.com>
2024-12-28 22:29:38 +0900

    init

---
+++ TurretKiller/Plugin.cs
@@ -0,0 +1,20 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+namespace FixedItemRotation
+{
+    [BepInPlugin("FixedItemRotation", "FixedItemRotation", "0.0.1")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new("FixedItemRotation");
+        internal static ManualLogSource logger;
+
+        private void Awake()
+        {
+            harmony.PatchAll();
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin FixedItemRotation is loaded!");
+
+        }
+    }
+}
---
+++ thunderstore/README.md
@@ -0,0 +1,3 @@
+# Fixed Item Rotation
+
+Fixes the angle to make it easier to organize items on the ship.
